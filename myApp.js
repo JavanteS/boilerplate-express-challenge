b131de88919ba4b32c5ff58ee3f94a0493a77456
javante_nelson <javantenelson@gmail.com>
2021-09-26 17:39:01 -0500

    practice routing

--- myApp.js
+++ myApp.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World")
+// console.log("Hello World")
+
+// app.get('/', function (req, res) {
+//     res.send('Hello Express')
+//   })
+
+var absolutePath = __dirname + /views/index.html
+
 
 app.get('/', function (req, res) {
-    res.send('Hello Express')
+    res.sendFile(absolutePath)
   })
-
-
 
 
 
